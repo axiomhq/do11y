4ae74ceb9c79d05100da1d1fe59595e229f57bae
Mano Toth <mano@axiom.co>
2026-02-24 11:57:27 +0100

    Add copy button to Nextra test site

--- tests/sites/nextra/next.config.mjs
+++ tests/sites/nextra/next.config.mjs
@@ -1,4 +1,4 @@
 import nextra from 'nextra';
 
-const withNextra = nextra({ theme: 'nextra-theme-docs', themeConfig: './theme.config.jsx' });
+const withNextra = nextra({ theme: 'nextra-theme-docs', defaultShowCopyCode: true, themeConfig: './theme.config.jsx' });
 export default withNextra({ reactStrictMode: true });
