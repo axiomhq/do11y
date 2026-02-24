358ef48037bdb5ecd192c46781bbb87187c038dd
Mano Toth <mano@axiom.co>
2026-02-24 12:02:00 +0100

    Update Next for Nextra

--- tests/sites/nextra/next.config.mjs
+++ tests/sites/nextra/next.config.mjs
@@ -1,4 +1,10 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import nextra from 'nextra';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const withNextra = nextra({ theme: 'nextra-theme-docs', defaultShowCopyCode: true, themeConfig: './theme.config.jsx' });
-export default withNextra({ reactStrictMode: true });
+export default withNextra({
+  reactStrictMode: true,
+  outputFileTracingRoot: __dirname,
+});
