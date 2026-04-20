7faa4d7b2bd66ea2c710fbcd9cb3c9dd66b0043f
Mano Toth <mano@axiom.co>
2026-04-20 12:25:35 +0200

    Bump to v0.0.5

--- dist/do11y.js
+++ dist/do11y.js
@@ -1,6 +1,6 @@
 (function() {
 	//#region src/do11y.ts
-	const VERSION = "0.0.4";
+	const VERSION = "0.0.5";
 	const _alreadyLoaded = !!window.__axiomDo11yInitialized;
 	window.__axiomDo11yInitialized = true;
 	const config = {
